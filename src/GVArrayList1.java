4a32a00491763808e148f2ee993819ef16c9437c
gurvirde1 <gurpreet.virdee@digitaslbi.com>
2017-02-26 17:31:22 +0000

    Create GVArrayList1.java

---
+++ src/GVArrayList1.java
@@ -0,0 +1,18 @@
+class GVArrayList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+			
+		System.out.println("Hello World, again!"); // Display the string.
+
+		int[] numbers = {1,2,3};
+		
+		System.out.print(numbers[0]); // Display the string.
+		System.out.print(" "); // Display the string.
+		System.out.println(numbers[1]); // Display the string.
+    System.out.print(" "); // Display the string.
+		System.out.println(numbers[2]); // Display the string.
+	}
+	
+}

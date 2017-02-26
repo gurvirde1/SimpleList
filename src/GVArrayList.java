5ecdb6fcffee3567800b97617dc42aa603a5720d
Gurpreet Virdee <gurvirde1@publicisgroupe.net>
2017-02-26 17:09:59 +0000

    added GVArralList.java

---
+++ src/GVArrayList.java
@@ -0,0 +1,16 @@
+class GVArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+			
+		System.out.println("Hello World!"); // Display the string.
+
+		int[] numbers = {1,2};
+		
+		System.out.print(numbers[0]); // Display the string.
+		System.out.print(" "); // Display the string.
+		System.out.println(numbers[1]); // Display the string.
+	}
+	
+}
